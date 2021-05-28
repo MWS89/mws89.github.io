c69863d3e2e2cf10903cc16343db58f6daf76bad
MWS89 <84924237+MWS89@users.noreply.github.com>
2021-05-28 12:24:00 +0100

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -171,7 +171,7 @@ var app = {
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/assets/js/sw.js')
+        .register('/sw.js')
         .then(() => { console.log('Service Worker Registered'); });
 }
 
